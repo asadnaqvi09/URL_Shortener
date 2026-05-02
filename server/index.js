6241c8163979d24a006d277186a65ae499eee459
asad khan tanoli <nasad8569@gmail.com>
2026-05-02 10:17:23 +0500

    ...

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import path from 'path'
+import helmet from 'helmet'
 import { fileURLToPath } from 'url'
 import urlRoutes from './src/routes/url_routes.js'
 import analyticsRoutes from './src/routes/analytics_routes.js'
@@ -18,27 +19,26 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors(
     {
-        origin : `http://localhost:${PORT}`,
+        origin : true,
         credentials : true
     }
 ));
+app.use(helmet())
 
 // View Engines
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 // Static Files
-app.use(express.static(path.join(__dirname, 'client')))
+app.use(express.static(path.join(__dirname, 'views')))
 
 // Routes
 app.use('/api/v1/' , urlRoutes)
 app.use('/api/v1/analytics' , analyticsRoutes)
 app.use('/' , redirectRoutes)
-// Frotnend Route
 app.get('/', (req, res) => {
     res.render('index')
 })
-// SPA Fallback
 app.use((req, res, next) => {
     if (req.path.startsWith('/api') || req.path.startsWith('/js') || req.path.startsWith('/css') || req.path.startsWith('/images')) {
         return next()
